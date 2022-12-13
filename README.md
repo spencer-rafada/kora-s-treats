ccc41f916bdc5aa70f7672ec7d08bf5dcc9fa420
spencer-rafada <spencer.byui@gmail.com>
2022-12-13 09:45:57 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Overview
+
+As a Software Engineer, I do my best to learn the fundamental skills in software development. I have come from a background wherein I learned Front End Development with React initially. This project helped learn Software Development by using Vanilla JS, HTML, and CSS
+
+KoRa's Treats is a web app that I created initially to highlight my fiancee's passion in baking. You can start the website using Live Server. I used SCSS as the CSS Preprocessor for the styling. I created this project to be able to highlight some skills that I can use when I contact small business owner about developing their own business websites.
+
+The purppose of this software is to further my knowledge in fundamental Software Development such as DOM Manipulation, styling, and developing in JavaScript without any frameworks.
+
+# Web Pages
+
+* Home Page - It highlights all of the products from KoRa's Treats. It makes use of grid and flexbox layouting. There are simple animations during hover on the navigation bar and the products bar.
+* Products Page - It shows all of the products from KoRa's treats. It makes use of grid and overflow-x to scroll horizontally and view all of the products.
+* Contact Us Page - The review section is similar to the products page. As of now, the links are placeholders. However, I will update the link in the future if my fiancee decides to make it her own business .
+
+# Development Environment
+
+- VS Code
+- Live Server
+- HTML
+- JavaScript
+- CSS / SCSS
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+* [Best Looking CSS Animated Background Ideas/Examples](https://www.sliderrevolution.com/resources/css-animated-background/)
+* [Moving Petals Background](https://codepen.io/qqz/pen/GzyQVb)
+
+# Future Work
+
+* Integrate it to a Web Framework
+* Handle orders and payments
+* Improve UI/UX and SEO to gain more clients
---
+++ index.js
@@ -0,0 +1,297 @@
+var Products = new Object();
+Products = [
+  {
+    product_name: "Birthday Sweets and Treats - Bear Theme",
+    price: "",
+    short_description:
+      "A combination of cookies, cone dipped in icing, pretzels with white chocolate that is perfect for your child's next birthday.",
+    long_description: [
+      "This is combination of treats were ordered by one of our closest friends for her little boy's birthday.",
+      "Depending on the availability, you can request a special order for your child according to your own theme!",
+    ],
+    img: "./img/resized/rsz_1bday-1.jpg",
+    category: ["birthday"],
+  },
+  {
+    product_name: "Wedding Cupcakes",
+    price: "",
+    short_description: "The perfect cupcakes for your next anticipated event.",
+    long_description: [
+      "This cupcake was made with a swirl of icing on top topped with white chocolate pearls.",
+      "This order was ordered by my sister for her wedding day.",
+    ],
+    img: "./img/resized/rsz_1wedding-1.jpg",
+    category: ["wedding"],
+  },
+  {
+    product_name: "Wedding Bun Cake",
+    price: "",
+    short_description: "The best wedding bun cake for your special day.",
+    long_description: [
+      "This bun cake is topped with icing decorated with baby's breath flowers per requested by the bride for her special day.",
+      "This order was made by my sister for her wedding day.",
+    ],
+    img: "./img/resized/rsz_1wedding-2.jpg",
+    category: ["cake", "wedding"],
+  },
+  {
+    product_name: "Creme Brulee Puff Pastry",
+    price: "",
+    short_description:
+      "Satisfying Pastry filled with pudding topped with glazed that will satisfy your sweet tooth.",
+    long_description: [
+      "This pastry is filled with pudding inside and topped with a glaze that just hits the pallete.",
+      "This order was made for my friends.",
+    ],
+    img: "./img/resized/rsz_1treats-3.jpg",
+    category: ["treats", "pastry"],
+  },
+  {
+    product_name: "Strawberry Danish",
+    price: "",
+    short_description: "Danish pastry, strawberry jam, ",
+    long_description: [
+      "This easy and beautiful Strawberry Cream Cheese Danish is perfect for a breakfast treat, light dessert, or Sunday brunch. Simple, sweet, and only seven ingredients. You will be delighted in every way!",
+    ],
+    img: "./img/resized/rsz_1treats-4.jpg",
+    category: ["pastry"],
+  },
+  {
+    product_name: "Wedding Vanilla Cake",
+    price: "",
+    short_description: "Vanilla Cake, Multi-layered,",
+    long_description: [
+      "Ever seen a combination of cake and floral design? Here it is.",
+      "Get your own for your next special event!",
+    ],
+    img: "./img/resized/rsz_1wedding-3.jpg",
+    category: ["cake", "wedding"],
+  },
+  {
+    product_name: "Mangomisu Cake",
+    price: "",
+    short_description: "Mangoes, Tiramisu, Ladies Finger, ",
+    long_description: [
+      "Sweet and healthy.",
+      "Get your own taste of tiramisu mixed with mangoes instead of coffee.",
+    ],
+    img: "./img/resized/rsz_cake-5.jpg",
+    category: ["cake"],
+  },
+];
+
+var Reviews = new Object();
+Reviews = [
+  {
+    reviewer_name: "Chelice Ko Aguilar",
+    review_text:
+      "The cake she made for my wedding was so good! It really means a lot to me that my sister made my cake for my special day.",
+  },
+  {
+    reviewer_name: "Jenny Gonzales",
+    review_text: "Ang sarap sarap neto! Magaling na magaling yung baker!",
+  },
+  {
+    reviewer_name: "Kryssma Flores",
+    review_text: "Must buy treats! I highly recommend this one.",
+  },
+  {
+    reviewer_name: "Abish Corpuz",
+    review_text: "The cupcakes are so filling and so moist on the inside!",
+  },
+  {
+    reviewer_name: "Mhel Corpuz",
+    review_text: "Masarap na masarap ang mga ginagawa ni Hana! Must buy!",
+  },
+  {
+    reviewer_name: "Andoy Corpuz",
+    review_text: "Pinakain ni papa sakin yung gawa ni tita, ang sarap sobra!",
+  },
+  {
+    reviewer_name: "Mikee Rafada",
+    review_text: "Everything she bakes are so good and delicious!",
+  },
+];
+
+var Credits = new Object();
+Credits = [
+  {
+    credits_name: "Kryssma Flores",
+    feedback: "gave feedback",
+  },
+  {
+    credits_name: "Jenny Gonzales",
+    feedback: "gave feedback",
+  },
+  {
+    credits_name: "Jasmin Pioquento",
+    feedback: "designer of logo and social media icon assets",
+  },
+];
+
+const showProducts = () => {
+  Products.map((product) => {
+    // console.log(product);
+    const element = document.getElementById("products");
+    const div_product = document.createElement("div");
+    div_product.className = "product";
+    const div_product__info = document.createElement("div");
+    div_product__info.className = "product__info";
+    // Product Name
+    const product_name = document.createElement("h3");
+    product_name.innerHTML = product.product_name;
+    // Image
+    var img = new Image();
+    img.src = product.img;
+    // console.log(img);
+    // Short Description
+    const short_description = document.createElement("p");
+    const short_desc_value = document.createTextNode(product.short_description);
+    short_description.appendChild(short_desc_value);
+
+    div_product__info.appendChild(product_name);
+    div_product__info.appendChild(short_description);
+    div_product.appendChild(img);
+    div_product.appendChild(div_product__info);
+    element.appendChild(div_product);
+  });
+};
+
+const showReviews = () => {
+  Reviews.map((review) => {
+    const element = document.getElementById("reviews__scroll");
+    const div_review = document.createElement("div");
+    div_review.className = "review";
+    const reviewer_name = document.createElement("h3");
+    reviewer_name.innerHTML = review.reviewer_name;
+    const review_text = document.createElement("p");
+    const review_text_value = document.createTextNode(review.review_text);
+    review_text.appendChild(review_text_value);
+
+    const hr = document.createElement("hr");
+
+    div_review.appendChild(reviewer_name);
+    div_review.appendChild(review_text);
+    div_review.appendChild(hr);
+    element.appendChild(div_review);
+  });
+};
+
+const showReviewsWithImage = () => {
+  Reviews.map((review) => {
+    const element = document.getElementById("reviews__scroll");
+    const div_review = document.createElement("div");
+    div_review.className = "review";
+    const reviewer_name = document.createElement("h3");
+    reviewer_name.innerHTML = review.reviewer_name;
+    const review_text = document.createElement("p");
+    const review_text_value = document.createTextNode(
+      `"${review.review_text}"`
+    );
+    review_text.appendChild(review_text_value);
+    var img = new Image();
+    img.src = "./img/rsz_1about-us.jpg";
+
+    div_review.appendChild(img);
+    div_review.appendChild(reviewer_name);
+    div_review.appendChild(review_text);
+    element.appendChild(div_review);
+  });
+};
+
+const showTitle = () => {
+  const randomIndex = Math.floor(Math.random() * Products.length);
+  // console.log(randomIndex);
+  document.getElementById("hero__img").src = Products[randomIndex].img;
+  const title_h1 = document.getElementById("hero-msg__title");
+  title_h1.innerHTML = Products[randomIndex].product_name;
+  const short_desc = document.getElementById("hero-msg__info__desc");
+  short_desc.innerHTML = Products[randomIndex].short_description;
+};
+
+const loadCredits = () => {
+  const credits_div = document.getElementById("credits");
+  Credits.map((credit) => {
+    const credits_name = document.createElement("h2");
+    credits_name.innerHTML = credit.credits_name;
+    const feedback = document.createElement("p");
+    const feedback_value = document.createTextNode(credit.feedback);
+    const hr = document.createElement("hr");
+    feedback.appendChild(feedback_value);
+    credits_div.appendChild(credits_name);
+    credits_div.appendChild(feedback);
+    credits_div.appendChild(hr);
+  });
+};
+
+var petalPlayers = [];
+
+function animatePetals() {
+  var petals = document.querySelectorAll(".petal");
+
+  if (!petals[0].animate) {
+    var petalsContainer = document.getElementById("petals-container");
+    petalsContainer.prepend(
+      "Uh oh, it seems like your browser doesn't support Web Animations API yet. Have you tried this in Firefox or Chrome?"
+    );
+    return false;
+  }
+
+  for (var i = 0, len = petals.length; i < len; ++i) {
+    var petal = petals[i];
+    petal.innerHTML =
+      '<div class="rotate"><img src="https://qqz.works/wp-content/uploads/2021/08/petal.png" class="askew"></div>';
+    var scale = Math.random() * 0.8 + 0.2;
+
+    var player = petal.animate(
+      [
+        {
+          transform:
+            "translate3d(" + (i / len) * 100 + "vw,0,0) scale(" + scale + ")",
+          opacity: scale,
+        },
+        {
+          transform:
+            "translate3d(" +
+            ((i / len) * 100 + 10) +
+            "vw,150vh,0) scale(" +
+            scale +
+            ")",
+          opacity: 1,
+        },
+      ],
+      {
+        duration: Math.random() * 90000 + 3000,
+        iterations: Infinity,
+        delay: -(Math.random() * 5000),
+      }
+    );
+
+    petalPlayers.push(player);
+  }
+}
+
+// For index.html
+const showAll = () => {
+  showTitle();
+  showProducts();
+  showReviews();
+  animatePetals();
+};
+
+// For products.html
+const showAllProducts = () => {
+  showProducts();
+  animatePetals();
+};
+
+// For contactus.html
+const showAllContactUs = () => {
+  showReviewsWithImage();
+  animatePetals();
+};
+
+const showCredits = () => {
+  loadCredits();
+  animatePetals();
+};
